2c9e2eba60ae978c4fe318ee53662d8dbf26d281
Pablo Lopez <plopesc@gmail.com>
2010-05-20 09:45:56 +0200

    New implementation of cartociudad.geocode.js

--- source/mxn.cartociudad.geocode.js
+++ source/mxn.cartociudad.geocode.js
@@ -3,47 +3,28 @@ mxn.register('cartociudad', {
 MapstractionGeocoder: {
 	
 	init: function(element, api) {		
-	  var me = this;
-	  me.geocoders[api] = new GClientGeocoder();
+		this.geocoders[api] = new metodosCartociudad();
 	},
 	geocode: function(address){
-	  var return_location = {};
-	  var mapstraction_geocoder = this;
-	
-	  if (address.address == null || address.address == "")
-	  address.address = address.street + ", " + address.locality + ", " + address.region + ", " + address.country
-	  this.geocoders[this.api].getLocations(address.address, function(response) { mapstraction_geocoder.geocode_callback(response, mapstraction_geocoder); });
+		var return_location = {};
+		var mapstraction_geodocer = this;
+		
+		address.error=0; //creamos una variable para devolver errores
+			
+		this.geocoders[this.api].queryNomenclator(address);
+			
+		if (address.error !== 0) {
+			this.error_callback(address);
+		}
+		else {
+			this.geocoders[this.api].addressToMapstraction(address);
+			this.callback(address, this);
+		}
 	},
 	geocode_callback: function(response, mapstraction_geocoder){
-	  var return_location = {};
+		var return_location = {};
 		
-	  if (!response || response.Status.code != 200) {
-		mapstraction_geocoder.error_callback(response);
-	  } else {
-	    return_location.street = "";
-		return_location.locality = "";
-		return_location.region = "";
-		return_location.country = "";
-
-		var place = response.Placemark[0];
-		if(place.AddressDetails.Country.AdministrativeArea != null) {
-		  return_location.region = place.AddressDetails.Country.AdministrativeArea.AdministrativeAreaName;
-				
-		  if(place.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea != null) {
-			if(place.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality != null) {
-			  return_location.locality = place.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.LocalityName;
-              if(place.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare != null)
-			    return_location.street = place.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare.ThoroughfareName;
-		    }
-					
-		  }
-				
-	  	}
-		return_location.country = place.AddressDetails.Country.CountryNameCode;
-		return_location.point = new mxn.LatLonPoint(place.Point.coordinates[1],
-		place.Point.coordinates[0]);
-		mapstraction_geocoder.callback(return_location);
-	  }
+		// TODO: Add provider code
 	}
 }
 })
