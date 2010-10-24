bd1a94580c9fa8484bf242e865258aa3e4cf4a53
Derek Fowler <dezfowler@gmail.com>
2010-10-24 21:43:06 +0100

    From plopesc: Fixed fail in addOverlay method in microsoft provider

--- source/mxn.microsoft.core.js
+++ source/mxn.microsoft.core.js
@@ -262,7 +262,7 @@ Mapstraction: {
 		var map = this.maps[this.api];
 		var layer = new VEShapeLayer(); 
 		var mlayerspec = new VEShapeSourceSpecification(VEDataType.GeoRSS, url, layer);
-	 	map.AddShapeLayer(layer);
+	 	map.ImportShapeLayerData(mlayerspec);
 	},
 
 	addTileLayer: function(tile_url, opacity, copyright_text, min_zoom, max_zoom) {
