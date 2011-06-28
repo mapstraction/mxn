a7a8dd9cce6ce49e56bc67dedde01b82ba62880f
vicchi <gary@vicchi.org>
2011-06-28 18:20:25 +0200

    Added support for the Ovi Maps API

--- license.js
+++ license.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) ${YEAR} Tom Carden, Steve Coast, Mikel Maron, Andrew Turner, Henri Bergius, Rob Moran, Derek Fowler
+Copyright (c) ${YEAR} Tom Carden, Steve Coast, Mikel Maron, Andrew Turner, Henri Bergius, Rob Moran, Derek Fowler, Gary Gale
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
