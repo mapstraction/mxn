6b0c0f8b8b7638632596f05f91c12de9f4447772
Greg Woods <greg@westlakedesign>
2011-11-07 10:05:28 -0500

    implemented geocode module for microsoft7 provider added api key to geocode constructor geocode function now accepts a string literal address in addition to address object and latlonpoint

--- source/mxn.geocoder.js
+++ source/mxn.geocoder.js
@@ -6,7 +6,7 @@
  * @private
  */
 var init = function() {
-	this.invoker.go('init');
+  this.invoker.go('init');
 };
 
 /**
@@ -16,30 +16,32 @@ var init = function() {
  * @param {String} api The API to use, currently only 'mapquest' is supported
  * @param {Function} callback The function to call when a geocode request returns (function(waypoint))
  * @param {Function} error_callback The optional function to call when a geocode request fails
+ * @param {String} key Optional api auth key. Currently only used for Microsoft v7.
  * @exports Geocoder as mxn.Geocoder
  */
-var Geocoder = mxn.Geocoder = function (api, callback, error_callback) {
-	this.api = api;
-	this.geocoders = {};
-	this.callback = callback;
-	this.error_callback = error_callback || function(){};
-	  
-	// set up our invoker for calling API methods
-	this.invoker = new mxn.Invoker(this, 'Geocoder', function(){ return this.api; });
-	init.apply(this);
+var Geocoder = mxn.Geocoder = function (api, callback, error_callback, key) { 
+  this.api = api;
+  this.geocoders = {};
+  this.callback = callback;
+  this.error_callback = error_callback || function(){};
+  this.key = (typeof(key) == 'string') ? key : false
+
+  // set up our invoker for calling API methods
+  this.invoker = new mxn.Invoker(this, 'Geocoder', function(){ return this.api; });
+  init.apply(this);
 };
 
 mxn.addProxyMethods(Geocoder, [
-	
-	/**
-	 * Geocodes the provided address.
-	 * @name mxn.Geocoder#geocode
-	 * @function
-	 * @param {Object} address Address hash, keys are: street, locality, region, country.
-	 */
-	'geocode',
-	
-	'geocode_callback'
+  
+  /**
+   * Geocodes the provided address.
+   * @name mxn.Geocoder#geocode
+   * @function
+   * @param {Object} address Address hash (street, locality, region, country), literal address as String, or LatLanPoint object
+   */
+  'geocode',
+  
+  'geocode_callback'
 
 ]);
 
@@ -49,12 +51,12 @@ mxn.addProxyMethods(Geocoder, [
  * @param {String} api The API to swap to
  */
 Geocoder.prototype.swap = function(api) {
-	if (this.api == api) { return; }
+  if (this.api == api) { return; }
 
-	this.api = api;
-	if (!this.geocoders.hasOwnProperty(this.api)) {
-		init.apply(this);
-	}
+  this.api = api;
+  if (!this.geocoders.hasOwnProperty(this.api)) {
+    init.apply(this);
+  }
 };
 
 })();
