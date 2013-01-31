cefa173bcfff0888c8ee773b62efa7f95eefa60c
vicchi <gary@vicchi.org>
2013-01-31 22:56:59 +0000

    MapQuest: Update addTileLayer function  to support new arguments (when/if implemented).

--- source/mxn.mapquest.core.js
+++ source/mxn.mapquest.core.js
@@ -355,7 +355,7 @@ Mapstraction: {
 		throw new Error('Mapstraction.addOverlay is not currently supported by provider ' + this.api);
 	},
 
-	addTileLayer: function(tile_url, opacity, copyright_text, min_zoom, max_zoom, map_type) {
+	addTileLayer: function(tile_url, opacity, label, attribution, min_zoom, max_zoom, map_type, subdomains) {
 		this._fireQueuedEvents();
 
 		throw new Error('Mapstraction.addTileLayer is not currently supported by provider ' + this.api);
