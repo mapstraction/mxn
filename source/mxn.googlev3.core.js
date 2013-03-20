567b26d6443ddd16af048f605f176c83cd253b10
vicchi <gary@vicchi.org>
2013-03-20 09:08:02 +0100

    Google V3 - fix multiple 'endPan' events firing during dragging

--- source/mxn.googlev3.core.js
+++ source/mxn.googlev3.core.js
@@ -89,15 +89,26 @@ Mapstraction: {
 				me.changeZoom.fire();
 			});
 		});
-
+		
 		// deal with map movement
+		var is_dragging = false;
+
+		google.maps.event.addListener(map, 'dragstart', function() {
+			is_dragging = true;
+		});
+
 		google.maps.event.addListener(map, 'dragend', function(){
 			me.moveendHandler(me);
 			me.endPan.fire();
+			is_dragging = false;
 		});
 		
 		google.maps.event.addListener(map, 'center_changed', function() {
-			me.endPan.fire();
+			if (!is_dragging) {
+				fireOnNextIdle.push(function() {
+					me.endPan.fire();
+				});
+			}
 		});
 		
 		// deal with initial tile loading
