5555e5b0eea2d03f007bbb0edeb7f139e0e839c3
gilesc50 <gilescollingwood@gmail.com>
2013-10-24 19:51:27 +0100

    updated to use the less compressed ol3 library until we get the exports we need implemented in ol3.

--- source/mxn.js
+++ source/mxn.js
@@ -832,7 +832,9 @@ mxn.Providers = {
 		],
 		'script': [
 			{
-				'src': 'http://ol3js.org/en/master/build/ol.js',
+
+			    //'src': 'http://ol3js.org/en/master/build/ol.js', //TODO: When ol3 exports the features we need for removing polylines
+				'src': 'http://ol3js.org/en/master/build/ol-simple.js',
 				'auth': false,
 				'authType': null
 			}
